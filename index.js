27e9d47d6e0ce8fe93a350d39aa5a080eaa93c64
Lauren Piontek <piontekle@gmail.com>
2018-10-12 21:25:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
+<<<<<<< HEAD
   for (let i=0; i < musicians.length; i++) {
   array.push(musicians[i] + " plays " + instruments[i])
   }
@@ -25,3 +26,13 @@ function theBeatlesPlay(musicians, instruments) {
     } while (number < 15);
     return array;
     }
+=======
+  for (let i=0; i < array.length; i++) {
+  if (i === 1) {
+  array.push(console.log(`${musicians.1} plays ${instruments.1}`))  
+  } else {
+    array.push(console.log(`${musicians.i} plays ${instruments.i}`))
+  }
+  }
+  }
+>>>>>>> e505d1b242045e5ecce842c43a860f828e45d6d7
